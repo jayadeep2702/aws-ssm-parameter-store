ed6d7aed82bcda53190b106b4ff7ba985e6e2dfe
akhil <akhil@gmail.com>
2023-09-15 22:05:06 +0530

    moving files

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "aws_ssm_parameter" "parameters" {
   name  = var.parameters[count.index].name
   value = var.parameters[count.index].value
   type  = "String"
-  key   = "43d9f06b-06fe-466a-b625-25c4fe8ea488"
+  key_id  = "43d9f06b-06fe-466a-b625-25c4fe8ea488"
 }
