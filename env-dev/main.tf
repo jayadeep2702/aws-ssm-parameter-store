7d5f3195e3cd137bc878e528940f47509d11a77f
akhil <akhil@gmail.com>
2023-09-15 22:11:24 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -2,6 +2,6 @@ parameters = [
   { name = "dev.frontend.catalogue_url" , value = "catalogue-dev.jdevops72.online" },
   { name = "dev.frontend.cart_url" , value = "cart-dev.jdevops72.online"},
   { name = "dev.frontend.user_url" , value = "user-dev.jdevops72.online"},
-  { name = "dev.shipping.catalogue_url" , value = "shipping-dev.jdevops72.online"},
-  { name = "dev.payment.catalogue_url" , value = "payment-dev.jdevops72.online"}
+  { name = "dev.frontend.shipping_url" , value = "shipping-dev.jdevops72.online"},
+  { name = "dev.frontend.payment_url" , value = "payment-dev.jdevops72.online"}
 ]
