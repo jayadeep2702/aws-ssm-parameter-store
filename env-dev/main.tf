42b91279411564655bdf34b745af9b375c59a6b6
akhil <akhil@gmail.com>
2023-09-17 14:55:23 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -11,11 +11,11 @@ parameters = [
   { name = "dev.user.redis_host" , value = "redis-dev.jdevops72.online" },
   { name = "dev.user.mongodb_url" , value = "mongodb://mongodb-dev.jdevops72.online:27017/users" },
   { name = "dev.cart.redis_host" , value = "redis-dev.jdevops72.online" },
-  { name = "dev.cart.catalogue_host" , value = "catalogue-dev.jdevops72.online" },
   { name = "dev.cart.catalogue_port" , value = "8080" },
   { name = "dev.cart.catalogue_host" , value = "catalogue-dev.jdevops72.online" },
   { name = "dev.shipping.cart_endpoint" , value = "cart-dev.jdevops72.online:8080" },
-  { name = "dev.shipping.db_host" , value = "mysql-dev.jdevops72.online" }
-
+  { name = "dev.shipping.db_host" , value = "mysql-dev.jdevops72.online" },
+  { name = "dev.shipping.db_user" , value = "roboshop" },
+  { name = "dev.shipping.db_pass" , value = "Roboshop@1" }
 ]
 
