258096c83d99f5e25c0e7ae695ea2c5c847f9744
akhil <akhil@gmail.com>
2023-09-17 14:41:56 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -12,7 +12,10 @@ parameters = [
   { name = "dev.user.mongodb_url" , value = "mongodb://mongodb-dev.jdevops72.online:27017/users" },
   { name = "dev.cart.redis_host" , value = "redis-dev.jdevops72.online" },
   { name = "dev.cart.catalogue_host" , value = "catalogue-dev.jdevops72.online" },
-  { name = "dev.cart.catalogue_port" , value = "8080" }
+  { name = "dev.cart.catalogue_port" , value = "8080" },
+  { name = "dev.cart.catalogue_host" , value = "catalogue-dev.jdevops72.online" },
+  { name = "dev.shipping.cart_endpoint" , value = "cart-dev.jdevops72.online:8080" },
+  { name = "dev.shipping.db_host" , value = "mysql-dev.jdevops72.online" }
 
 ]
 
