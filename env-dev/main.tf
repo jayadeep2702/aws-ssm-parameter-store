bf44f56e1b4edf47f6bfef2860b36d4eef784196
akhil kumar <akhil@gmail.com>
2023-10-13 15:07:28 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -30,5 +30,7 @@ parameters = [
 passwords = [
  { name = "dev.payment.amqp_pass" , value = "roboshop123" },
   { name = "dev.shipping.db_pass" , value = "Roboshop@1" },
+  { name = "dev.docdb.db_user" , value = "roboshop" },
+  { name = "dev.docdb.db_pass" , value = "roboshop123" }
 
 ]
