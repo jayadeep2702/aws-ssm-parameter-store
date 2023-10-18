4d2f50725104a548c7511fefbc7f6102519846ca
akhil kumar <akhil@gmail.com>
2023-10-18 16:10:20 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -5,9 +5,7 @@ parameters = [
   { name = "dev.frontend.shipping_url" , value = "http://shipping-dev.jdevops72.online:8080/" },
   { name = "dev.frontend.payment_url" , value = "http://payment-dev.jdevops72.online:8080/" },
   { name = "dev.catalogue.mongodb_endpoint" , value = "mongodb-dev.jdevops72.online" },
-  { name = "dev.catalogue.mongodb" , value = "MONGO=true" },
-  { name = "dev.catalogue.mongodb_url" , value = "mongodb://mongodb-dev.jdevops72.online:27017/catalogue" },
-  { name = "dev.user.mongodb" , value = "MONGO=true" },
+  { name = "dev.catalogue.mongodb" , value = "DOCUMENTDB=true" },
   { name = "dev.user.redis_host" , value = "redis-dev.jdevops72.online" },
   { name = "dev.user.mongodb_url" , value = "mongodb://mongodb-dev.jdevops72.online:27017/users" },
   { name = "dev.user.mongodb_endpoint" , value = "mongodb-dev.jdevops72.online" },
@@ -23,6 +21,7 @@ parameters = [
   { name = "dev.payment.user_port" , value = "8080" },
   { name = "dev.payment.amqp_host" , value = "rabbitmq-dev.jdevops72.online" },
   { name = "dev.payment.amqp_user" , value = "roboshop" },
+  { name = "dev.docdb.db_endpoint", value = "docdb-dev.cluster-cz0mqmd5k99c.us-east-1.docdb.amazonaws.com" }
 
 
 ]
@@ -34,6 +33,7 @@ passwords = [
   { name = "dev.docdb.db_pass" , value = "roboshop123" },
   { name = "dev.rds.db_user" , value = "roboshop" },
   { name = "dev.rds.db_pass" , value = "roboshop123" },
-  { name = "dev.rabbitmq.rabbitmq_appuser_password" , value = "roboshop123" }
+  { name = "dev.rabbitmq.rabbitmq_appuser_password" , value = "roboshop123" },
+  { name = "dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123456@docdb-dev.cluster-cz0mqmd5k99c.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false" },
 
 ]
