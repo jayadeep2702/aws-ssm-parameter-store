62905497b234af0a47ab8323fda51f5165be6acb
akhil kumar <akhil@gmail.com>
2023-10-20 14:28:26 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -5,7 +5,7 @@ parameters = [
   { name = "dev.frontend.shipping_url" , value = "http://shipping-dev.jdevops72.online:8080/" },
   { name = "dev.frontend.payment_url" , value = "http://payment-dev.jdevops72.online:8080/" },
   { name = "dev.catalogue.mongodb_endpoint" , value = "mongodb-dev.jdevops72.online" },
-  { name = "dev.catalogue.mongodb" , value = "DOCUMENTDB=true" },
+  { name = "dev.catalogue.mongo" , value = "DOCUMENTDB=true" },
   { name = "dev.user.mongo", value = "DOCUMENTDB=true" },
   { name = "dev.user.redis_host" , value = "redis-dev.jdevops72.online" },
   { name = "dev.user.mongodb_url" , value = "mongodb://mongodb-dev.jdevops72.online:27017/users" },
