fd1218841639156a5e1560ff33c4c95cd7eb1c76
akhil kumar <akhil@gmail.com>
2023-10-21 16:50:52 +0530

    moving files

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -1,17 +1,17 @@
 parameters = [
-  { name = "dev.frontend.catalogue_url" , value = "http://catalogue-dev.jdevops72.online:8080/" },
-  { name = "dev.frontend.cart_url" , value = "http://cart-dev.jdevops72.online:8080/" },
-  { name = "dev.frontend.user_url" , value = "http://user-dev.jdevops72.online:8080/" },
-  { name = "dev.frontend.shipping_url" , value = "http://shipping-dev.jdevops72.online:8080/" },
-  { name = "dev.frontend.payment_url" , value = "http://payment-dev.jdevops72.online:8080/" },
+  { name = "dev.frontend.catalogue_url" , value = "http://catalogue-dev.jdevops72.online:80/" },
+  { name = "dev.frontend.cart_url" , value = "http://cart-dev.jdevops72.online:80/" },
+  { name = "dev.frontend.user_url" , value = "http://user-dev.jdevops72.online:80/" },
+  { name = "dev.frontend.shipping_url" , value = "http://shipping-dev.jdevops72.online:80/" },
+  { name = "dev.frontend.payment_url" , value = "http://payment-dev.jdevops72.online:80/" },
   { name = "dev.catalogue.mongo_endpoint" , value = "mongodb-dev.jdevops72.online" },
   { name = "dev.catalogue.mongo" , value = "DOCUMENTDB=true" },
   { name = "dev.user.mongo", value = "DOCUMENTDB=true" },
-  { name = "dev.user.redis_host" , value = "redis-dev.jdevops72.online" },
+  { name = "dev.user.redis_host" , value = "elasticache-dev-elasticache.afinsg.ng.0001.use1.cache.amazonaws.com" },
   { name = "dev.user.mongodb_url" , value = "mongodb://mongodb-dev.jdevops72.online:27017/users" },
   { name = "dev.user.mongodb_endpoint" , value = "mongodb-dev.jdevops72.online" },
-  { name = "dev.cart.redis_host" , value = "redis-dev.jdevops72.online" },
-  { name = "dev.cart.catalogue_port" , value = "8080" },
+  { name = "dev.cart.redis_host" , value = "elasticache-dev-elasticache.afinsg.ng.0001.use1.cache.amazonaws.com" },
+  { name = "dev.cart.catalogue_port" , value = "80" },
   { name = "dev.cart.catalogue_host" , value = "catalogue-dev.jdevops72.online" },
   { name = "dev.shipping.cart_endpoint" , value = "cart-dev.jdevops72.online:8080" },
   { name = "dev.shipping.db_host" , value = "mysql-dev.jdevops72.online" },
@@ -36,5 +36,5 @@ passwords = [
   { name = "dev.rds.db_pass" , value = "roboshop123" },
   { name = "dev.rabbitmq.rabbitmq_appuser_password" , value = "roboshop123" },
   { name = "dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cz0mqmd5k99c.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false" },
-
+  { name = "dev.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cz0mqmd5k99c.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false" }
 ]
